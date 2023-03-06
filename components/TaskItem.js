9209ea84ab3d2b786a8a57db2f4583a037421d9d
Elkayam Melesse <elka@duck.com>
2023-03-06 16:06:43 +0200

    press effect

--- components/TaskItem.js
+++ components/TaskItem.js
@@ -2,23 +2,31 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function TaskItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.taskItem}>
+    <View style={styles.taskItem}>
+      <Pressable
+        android_ripple={{ color: "#370d5e" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.taskText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   taskItem: {
     margin: 6,
-    padding: 6,
     borderRadius: 4,
     backgroundColor: "blueviolet",
   },
 
+  pressedItem: {
+    opacity: 0.5,
+  },
+
   taskText: {
+    padding: 6,
     color: "white",
   },
 });
